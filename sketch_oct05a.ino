d13e517e293ec41962c597d05afd99ebe95d0375
Benson0409 <83742453+Benson0409@users.noreply.github.com>
2021-10-05 10:21:41 +0800

    Add files via upload

---
+++ sketch_oct05a.ino
@@ -0,0 +1,31 @@
+#define Button_Pin 12
+#define LED_Pin 14
+
+int flashMode[3][2]={{100,100},{1500,250},{500,1000}};
+int modeIndex=0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(Button_Pin,INPUT);
+  pinMode(LED_Pin,OUTPUT);
+
+}
+bool btnPressed =false;
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(Button_Pin)==HIGH && btnPressed==false){
+    btnPressed=true;
+    modeIndex+=1;
+    if(modeIndex>=3)modeIndex=0;
+  }
+  else if(digitalRead(Button_Pin)==LOW && btnPressed==true){
+    btnPressed=false;
+  }
+  digitalWrite(LED_Pin,HIGH);
+  delay(flashMode[modeIndex][0]);
+  digitalWrite(LED_Pin,LOW);
+  delay(flashMode[modeIndex][1]);
+  
+
+}
